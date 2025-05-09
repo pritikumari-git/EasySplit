ee5f2cae63c9075e09817855ab2241e40dfa8e13
Priti Kumari <115141959+pritikumari-git@users.noreply.github.com>
2025-05-09 15:24:41 +0530

    Update api.jsx

--- EasySplit_client-main/api.jsx
+++ EasySplit_client-main/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://blog-backend-s7km.onrender.com";
+axios.defaults.baseURL = "https://easysplit-backend-m47s.onrender.com";
 // axios.defaults.baseURL = "http://localhost:5000/api";
 
 const api = axios.create({
